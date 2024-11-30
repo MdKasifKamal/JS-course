15d1fdb06feadba747f072c05b925bce348dd1b7
MdKasifKamal <mohdkasifkamal680@gmail.com>
2024-11-30 19:48:50 +0000

    variable

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"
+
+console.log(3+
+    3) // code readability should be high
+
+console.log("kasif")
+
+let name = "Kasif"
+let age = 19
+let isloggedIn = false
+
+//number => 2 to power 53
+//Bigint
+//string=>""
+//boolean =>true/false
+//null=>standalone value
+///undefined
+//symbol => unique
+//object
+console.log(typeof null) //object
