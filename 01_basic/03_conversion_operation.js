01e13ca829624dac4446c940962769556e7ede54
MdKasifKamal <mohdkasifkamal680@gmail.com>
2024-12-09 11:40:16 +0000

    conversion_different_datatype

---
+++ 01_basic/03_conversion_operation.js
@@ -0,0 +1,28 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let scoree = 33
+console.log(scoree)
+
+console.log(typeof scoree);
+console.log(typeof(scoree));
+
+let valueInString = String(scoree)
+console.log(typeof valueInString);
+// null ki value 0 undefine ki value NAN and string is not convert in num so ans is NAN
+// "33"=>33
+// "33abc=>NaN
+// true=>1 false=>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false
+// "" => false in boolean empty is false
+//"kasif" => true in bolean write anything is true
